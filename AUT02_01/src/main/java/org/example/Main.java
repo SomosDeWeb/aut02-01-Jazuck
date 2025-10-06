ca0d2d6f813c229c698bc2497d065e09d65e68e8
Jazuck <Alejandropalancares@gmail.com>
2025-10-06 13:11:55 +0100

    Menu

--- AUT02_01/src/main/java/org/example/Main.java
+++ AUT02_01/src/main/java/org/example/Main.java
@@ -1,17 +1,73 @@
 package org.example;
 
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("=== GESTOR DE ESTUDIANTES ===");
+        boolean continuar = true;
+        int opcion;
+
+        do{
+            mostrarMenu();
+            opcion = leerEntero("Seleccione una opción: ");
+
+            switch (opcion) {
+                case 1:
+                    aniadirEstudiante();
+                    break;
+                case 2:
+                    listarEstudiantes();
+                    break;
+                case 3:
+                    buscarNombre();
+                    break;
+                case 4:
+                    calcularMedia();
+                    break;
+                case 5:
+                    mejorEstudiante();
+                    break;
+                case 6:
+                    continuar = false;
+                    System.out.println("Has salido del programa...");
+                    break;
+                default:
+                    System.out.println("Opción inválida, intente de nuevo.");
+            }
+        }while (continuar);
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("1. Añadir estudiante");
+        System.out.println("2. Listar estudiantes");
+        System.out.println("3. Buscar por nombre");
+        System.out.println("4. Calcular nota media general");
+        System.out.println("5. Mostrar mejor estudiante");
+        System.out.println("6. Salir");
+    }
+
+    private static int leerEntero(String mensaje){
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        while (true) {
+            System.out.print(mensaje);
+            try {
+                numero = Integer.parseInt(sc.nextLine());
+                return numero;
+            } catch (NumberFormatException e) {
+                System.out.println("Introduzca un número válido.");
+            }
         }
     }
+
+    private static void aniadirEstudiante() {}
+    private static void listarEstudiantes() {}
+    private static void buscarNombre() {}
+    private static void calcularMedia() {}
+    private static void mejorEstudiante() {}
 }
